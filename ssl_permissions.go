e9ea8f38e86159ec761329ab13e30e74e99f7189
Eric Culp <eculperic@gmail.com>
2019-06-29 14:15:58 -0700

    Allow group permissions in ssl keys

--- ssl_permissions.go
+++ ssl_permissions.go
@@ -13,7 +13,7 @@ func sslKeyPermissions(sslkey string) error {
 	if err != nil {
 		return err
 	}
-	if info.Mode().Perm()&0077 != 0 {
+	if info.Mode().Perm()&0007 != 0 {
 		return ErrSSLKeyHasWorldPermissions
 	}
 	return nil
